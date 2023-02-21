52dbd6e0e47b722aae42f689e05bb5e548727eb0
sharpenedblade <sharpenedblade@proton.me>
2023-02-20 23:38:26 -0800

    Reduce logs

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 set -eu -o pipefail
 
-dnf install -y git pykickstart lorax anaconda lorax-lmc-novirt
+dnf install -y --quiet git pykickstart lorax anaconda lorax-lmc-novirt
 
-git clone --single-branch --branch f37 https://pagure.io/fedora-kickstarts.git /var/fedora-kickstarts
+git clone --quiet --single-branch --depth 1 --branch f37 https://pagure.io/fedora-kickstarts.git /var/fedora-kickstarts
 cd /var/fedora-kickstarts
 cp -rfv "/repo"/*.ks ./
 sudo ksflatten -c t2linux-fedora-workstation-live.ks -o flat.ks
 
-livemedia-creator --ks flat.ks --no-virt --resultdir /var/lmc --project Fedora-Workstation-T2Linux-Live --make-iso --volid Fedora-WS-T2-Live-37-1.7 --iso-only --iso-name Fedora-Workstation-T2Linux-Live-x86_64-37-1.7.iso --releasever 37 --macboot
+livemedia-creator --ks flat.ks --no-virt --resultdir /var/lmc --project Fedora-Workstation-T2Linux-Live --make-iso --volid Fedora-WS-T2-Live-37-1.7 --iso-only --iso-name Fedora-Workstation-T2Linux-Live-x86_64-37-1.7.iso --releasever 37
 
 cp -rfv /var/lmc/*.iso "/repo"/
 cd "/repo"
