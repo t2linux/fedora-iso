dd25e016fb2444ec8c9c5023c0350b3a360d4dd6
sharpenedblade <sharpenedblade@proton.me>
2023-10-01 15:39:49 -0700

    Remove large (>2GB) ISO release support

--- build.sh
+++ build.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/bash
 
-cd /repo
-cd fedora-kickstarts
-cp -rfv /repo/*.ks ./
+cd /repo/fedora-kickstarts
+cp -rfv /repo/*.ks .
 sudo ksflatten -c t2linux-fedora-workstation-live.ks -o flat.ks
 
 livemedia-creator --ks flat.ks --no-virt --resultdir /var/lmc --project Fedora-Workstation-Live-t2linux --make-iso --volid Fedora-WS-Live-t2-38-3.0.0 --iso-only --iso-name Fedora-Workstation-Live-t2linux-x86_64-38-3.0.0.iso --releasever 38
 
-cp -rfv /var/lmc/*.iso /repo/
-cd /repo
-
-mkdir -p _output
-if (( $(stat -c%s *.iso) > 199999999 )); then
-    mv *.iso _output/
-else
-    split -b 2000M -x ./*.iso  ./_output/Fedora-Workstation-Live-t2linux-x86_64-38-2.0.0.iso. 
-fi
+mkdir -p _output && mv /var/lmc/*.iso _output/
