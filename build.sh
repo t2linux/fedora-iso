df86bd0c4f6b7d02369ea5c40ef9b8db22d5afbd
sharpenedblade <sharpenedblade@proton.me>
2023-10-01 14:13:28 -0700

    Simplify build script and workflow
    This moves `fedora-kickstarts` to a git submodule. Builds are run in
    the `t2linux:fedora-dev` container with pre-installed dependencies. CI
    is only run for branch tips.

--- build.sh
+++ build.sh
@@ -1,22 +1,18 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-set -eu -o pipefail
-
-dnf install -y --quiet git pykickstart lorax anaconda lorax-lmc-novirt
-
-git clone --quiet --single-branch --depth 1 --branch f38 https://pagure.io/fedora-kickstarts.git /var/fedora-kickstarts
-cd /var/fedora-kickstarts
-cp -rfv "/repo"/*.ks ./
+cd /repo
+cd fedora-kickstarts
+cp -rfv /repo/*.ks ./
 sudo ksflatten -c t2linux-fedora-workstation-live.ks -o flat.ks
 
 livemedia-creator --ks flat.ks --no-virt --resultdir /var/lmc --project Fedora-Workstation-Live-t2linux --make-iso --volid Fedora-WS-Live-t2-38-3.0.0 --iso-only --iso-name Fedora-Workstation-Live-t2linux-x86_64-38-3.0.0.iso --releasever 38
 
-cp -rfv /var/lmc/*.iso "/repo"/
-cd "/repo"
+cp -rfv /var/lmc/*.iso /repo/
+cd /repo
 
-mkdir -p ./output
+mkdir -p _output
 if (( $(stat -c%s *.iso) > 199999999 )); then
-    mv *.iso ./output/
+    mv *.iso _output/
 else
-    split -b 2000M -x ./*.iso  ./output/Fedora-Workstation-Live-t2linux-x86_64-38-2.0.0.iso. 
+    split -b 2000M -x ./*.iso  ./_output/Fedora-Workstation-Live-t2linux-x86_64-38-2.0.0.iso. 
 fi
