542b68698307b92d8e1ad0ff586974be958f64d5
Neal Gompa <ngompa@fedoraproject.org>
2024-03-26 08:00:55 -0400

    uki: Take 2 to drop custom logic for discoverable partitions
    Since kiwi v10.0.10, it automatically sets the partition GUIDs to
    values from the UAPI group's discoverable partition standard.

--- uki-editbootconfig.sh
+++ uki-editbootconfig.sh
@@ -3,8 +3,8 @@ echo "###" "$0" "$@"
 
 # set arch-specific variables
 case "$(uname -m)" in
-    aarch64) arch="aa64"; ARCH="AA64"; uuid="b921b045-1df0-41c3-af44-4c6f280d3fae"; rootfs="2";;
-    x86_64)  arch="x64";  ARCH="X64";  uuid="4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709"; rootfs="3";;
+    aarch64) arch="aa64"; ARCH="AA64";;
+    x86_64)  arch="x64";  ARCH="X64";;
 esac
 
 # figure where shim.efi and BOOT.CSV are located
@@ -24,15 +24,6 @@ for uki in lib/modules/*/vmlinuz*.efi; do
     echo "shim${arch}.efi,$ver,\\EFI\\Linux\\${ver}.efi ,Comment" \
         | iconv -f utf-8 -t ucs-2le >> "$csv"
 done
-
-# kiwi doesn't setup discoverable partitions, so fixup after the fact
-# here.  The UKI depends on that to find the root filesystem.
-#  * The image is loop-mounted, partitions:
-#    - biosboot (on x86 only, can this be disabled?).
-#    - EFI ESP.
-#    - root filesystem (this needs fixup).
-echo "# hack: rootfs: $uuid"
-sfdisk --part-type /dev/loop0 "$rootfs" "$uuid"
 
 # bz2240989: shim has a hard dependency on grub.  grub has a hard
 # dependency on dracut.  Ideally we would simply not install
