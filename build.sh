8e6f074956d06f17f8835b7ae7ace932308d9f48
sharpenedblade <sharpenedblade@proton.me>
2025-04-15 19:33:28 -0700

    ci: Fix build.sh

--- build.sh
+++ build.sh
@@ -4,21 +4,22 @@ set -e
 release="0"
 profiles=( "Workstation-Live" )
 
-kiwi_dir="/repo/fedora-kiwi-descriptions"
+repo_dir="$PWD"
+kiwi_dir="${repo_dir}/fedora-kiwi-descriptions"
 
-mkdir -p /repo/builddir/iso
 function cleanup {
-  rm -rf "/repo/outdir-build"
+  rm -rf "${repo_dir}/outdir-build"
 }
 trap cleanup EXIT
 
 for profile in "${profiles[@]}"; do
-    rm -rf "/repo/outdir-build"
-    "${kiwi_dir}"/kiwi-build \
-        --kiwi-file="${kiwi_dir}/Fedora.kiwi" \
+    rm -rf "${repo_dir}/outdir-build"
+    cd "${kiwi_dir}"
+    ./kiwi-build \
+        --kiwi-file="Fedora.kiwi" \
         --image-type=iso \
         --image-profile="${profile}" \
         --image-release "${release}" \
-        --output-dir "/repo/outdir"
-    find /repo/outdir/*.iso -size +2G -exec sh -c 'split -b 1999M -x "$1" "$1". && rm "$1"' shell {} \;
+        --output-dir "${repo_dir}/outdir"
+    find "${repo_dir}/outdir"/*.iso -size +2G -exec sh -c 'split -b 1999M -x "$1" "$1". && rm "$1"' shell {} \;
 done
